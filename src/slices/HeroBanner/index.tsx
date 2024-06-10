66736c3d1b245f8b3b59e8a02ff47e59d7e421e1
Jannik <j.fuhrmann@micromata.de>
2024-06-10 22:14:58 +0200

    hero banner text-center

--- src/slices/HeroBanner/index.tsx
+++ src/slices/HeroBanner/index.tsx
@@ -17,23 +17,23 @@ const HeroBanner = ({ slice }: HeroBannerProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="max-h-[600px]"
+      className="h-[500px]"
     >
-      <div className="relative w-full">
+      <div className="relative w-full h-[500px]">
         <PrismicNextImage
           layout="responsive"
           field={slice.primary.image}
           width={slice.primary.image.dimensions?.width}
           height={slice.primary.image.dimensions?.height}
-          className="w-full grayscale-[40%] max-h-[600px] object-cover"
+          className="w-auto h-auto max-w-full max-h-full grayscale-[40%]"
         />
       </div>
-      <div className="absolute top-[50%] -translate-y-[75%] w-full flex flex-col justify-center items-center gap-4">
+      <div className="absolute top-[25%] -translate-y-[25%] w-full flex flex-col justify-center items-center gap-4">
         <h1 className="text-5xl text-white font-bold">
           {slice.primary.heading}
         </h1>
         <span className="bg-dunkelblau rounded-full w-20 h-2" />
-        <div className="text-gray-100 font-bold text-3xl text-balance max-w-md">
+        <div className="text-gray-100 font-bold text-3xl text-center max-w-md ">
           {slice.primary.subheading}
         </div>
         {isFilled.keyText(slice.primary.link_label) && (
