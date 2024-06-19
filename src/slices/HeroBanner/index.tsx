e859808076fa2df9a343ce5940d9981e356d6b2f
Jannik <j.fuhrmann@micromata.de>
2024-06-19 18:29:12 +0200

    fix herobanner for real

--- src/slices/HeroBanner/index.tsx
+++ src/slices/HeroBanner/index.tsx
@@ -27,22 +27,22 @@ const HeroBanner = ({ slice }: HeroBannerProps): JSX.Element => {
           height={slice.primary.image.dimensions?.height}
           className="object-cover"
         />
-      </div>
-      <div className="absolute top-[50%] -translate-y-[50%] w-full flex flex-col justify-center items-center gap-4">
-        <div className="text-white text-center max-w-md">
-          <RichTextWithComponents richText={slice.primary.heading_richtext} />
+        <div className="absolute top-[25%] w-full flex flex-col justify-center items-center gap-4">
+          <div className="text-white text-center max-w-md">
+            <RichTextWithComponents richText={slice.primary.heading_richtext} />
+          </div>
+          <span className="bg-dunkelblau rounded-full w-20 h-2" />
+          <div className="text-white text-center max-w-md">
+            <RichTextWithComponents
+              richText={slice.primary.subheading_richtext}
+            />
+          </div>
+          {isFilled.keyText(slice.primary.link_label) && (
+            <ButtonLink field={slice.primary.link} className="text-lg">
+              {slice.primary.link_label}
+            </ButtonLink>
+          )}
         </div>
-        <span className="bg-dunkelblau rounded-full w-20 h-2" />
-        <div className="text-white text-center max-w-md">
-          <RichTextWithComponents
-            richText={slice.primary.subheading_richtext}
-          />
-        </div>
-        {isFilled.keyText(slice.primary.link_label) && (
-          <ButtonLink field={slice.primary.link} className="text-lg">
-            {slice.primary.link_label}
-          </ButtonLink>
-        )}
       </div>
     </section>
   );
