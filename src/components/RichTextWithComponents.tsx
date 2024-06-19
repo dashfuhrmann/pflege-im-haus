b27ad05e8b507008f7e50790745a8f9620e1f01d
Jannik <j.fuhrmann@micromata.de>
2024-06-19 18:35:39 +0200

    change values

--- src/components/RichTextWithComponents.tsx
+++ src/components/RichTextWithComponents.tsx
@@ -11,12 +11,12 @@ const RichTextWithComponents = (props: { richText: RichTextField }) => {
         paragraph: ({ children }) => (
           <p className="text-balance text-lg">{children}</p>
         ),
-        heading1: ({ children }) => <h1 className="text-4xl">{children}</h1>,
-        heading2: ({ children }) => <h2 className="text-3xl">{children}</h2>,
-        heading3: ({ children }) => <h3 className="text-2xl">{children}</h3>,
-        heading4: ({ children }) => <h4 className="text-xl">{children}</h4>,
-        heading5: ({ children }) => <h5 className="text-lg">{children}</h5>,
-        heading6: ({ children }) => <h6 className="text-base">{children}</h6>,
+        heading1: ({ children }) => <h1 className="text-5xl">{children}</h1>,
+        heading2: ({ children }) => <h2 className="text-4xl">{children}</h2>,
+        heading3: ({ children }) => <h3 className="text-3xl">{children}</h3>,
+        heading4: ({ children }) => <h4 className="text-2xl">{children}</h4>,
+        heading5: ({ children }) => <h5 className="text-xl">{children}</h5>,
+        heading6: ({ children }) => <h6 className="text-large">{children}</h6>,
         list: ({ children }) => <ol className="list-disc ml-4">{children}</ol>,
         oList: ({ children }) => (
           <ol className="list-decimal ml-4">{children}</ol>
