0650528689c5d2b5fcde044feba78eb47b004184
Jannik <j.fuhrmann@micromata.de>
2024-11-14 09:52:25 +0100

    all the button changes

--- src/slices/HeroBanner/index.tsx
+++ src/slices/HeroBanner/index.tsx
@@ -57,7 +57,7 @@ const HeroBanner = ({ slice }: HeroBannerProps): JSX.Element => {
           ) : (
             isFilled.keyText(slice.primary.link_label) && (
               <ButtonLink
-                className="text-xl text-center text-white bg-primary text-balance"
+                className="text-2xl text-center text-white bg-primary text-balance"
                 field={slice.primary.link}
               >
                 {slice.primary.link_label}
